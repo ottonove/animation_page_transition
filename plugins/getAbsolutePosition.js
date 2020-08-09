1da767eaa8573cef5106073de63e1ba3a1984d9a
ottonove <niigata01web@gmail.com>
2020-08-09 12:16:14 +0900

    getAbsolutePositionの処理をシンプルにした。

--- plugins/getAbsolutePosition.js
+++ plugins/getAbsolutePosition.js
@@ -1,21 +1,15 @@
 export const getAbsolutePosition = (elm) => {
     const drawer = document.getElementById("v-drawer")
-    const drawerWidth = drawer.style.transform.match(/-?\d+/)[0] === "0" ? drawer.clientWidth : 0
+    const drawerWidth = (drawer.style.transform.match(/-?\d+/)[0] === "0") && (document.getElementsByClassName("v-overlay--active").length === 0) ? drawer.clientWidth : 0
+    // drawerのtransformが「translateX(0%)」、且つ、drawerのoverlayが無い、という場合(表示領域の横幅が十分広い場合)、drawerのwidth分をleftから引く。
 
     const {left, top} = elm.getBoundingClientRect();
     const {left: bleft, top: btop} = document.body.getBoundingClientRect();
     
-    let resultLeft = 0
-    if(left - bleft - drawerWidth > 0){
-        resultLeft = left - bleft - drawerWidth
-    } else {
-        resultLeft = left - bleft
-    }
-
     const appbar = document.getElementById("v-appbar")
 
     return {
-        left: resultLeft,
+        left: left - bleft - drawerWidth,
         top: top - btop - appbar.clientHeight
     }
 }
